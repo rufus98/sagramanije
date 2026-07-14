16369328128c49eb26aa83de60ec504e9e0a9399
Matteo <matteodibs@gmail.com>
2026-07-14 15:43:11 +0200

    [feat] added pending api call loader

--- src/app/sagra/[id].tsx
+++ src/app/sagra/[id].tsx
@@ -9,13 +9,13 @@ import { mapUtils } from "@/utils/map-utils";
 import { useQuery } from "@tanstack/react-query";
 import { useLocalSearchParams } from "expo-router";
 import { MapPin } from "lucide-react-native";
-import { ScrollView, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, ScrollView, TouchableOpacity, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function SagraPage() {
     const { id, locandina, distanza } = useLocalSearchParams<{ id: string; locandina?: string; distanza?: string }>()
     const insets = useSafeAreaInsets()
-    const { data } = useQuery({
+    const { data, isPending } = useQuery({
         queryKey: ["sagra", id],
         queryFn: async () => await sagraService.getById(id),
     })
@@ -25,9 +25,15 @@ export default function SagraPage() {
             <SagraHero source={data?.locandina ?? locandina} />
             <BackButton />
             <ThemedView className="rounded-3xl -mt-8 flex-1 px-5 py-6">
-                <ScrollView>
-                    <SagraInfo sagra={data} distanza={distanza ? Number(distanza) : undefined} />
-                </ScrollView>
+                {isPending ? 
+                    <View className="flex-1 items-center justify-center">
+                        <ActivityIndicator color={Colors.primary} />
+                    </View>
+                :
+                    <ScrollView>
+                        <SagraInfo sagra={data} distanza={distanza ? Number(distanza) : undefined} />
+                    </ScrollView>
+                }
             </ThemedView>
 
             {(data && data.lat) && <View className="bg-white px-3 pt-4 mb-2" style={{paddingBottom: insets.bottom}}>
